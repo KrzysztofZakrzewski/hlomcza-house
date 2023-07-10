84b4c501e507b07e759561f4f3f0c2567e284b8b
Krzysztof Zakrzewski <krzysztof.zakrzewski@protonmail.com>
2023-07-10 02:37:26 +0200

    Added basic logic for showing pictures from gallery

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,31 @@
 // 'use strict';
+
+const photosArr = [];
+let photos = document.querySelectorAll('.img');
+const galleryContainer = document.querySelector('.gallery-container');
+const galleryWrapper = document.querySelector('.gallery-wrapper');
+let galleryPhoto = document.querySelector('.gallery__photo');
+
+let currentPhoto;
+
+function colectAllPhotos() {}
+
+photos.forEach((photo, index) => {
+	const showPhotoInPopup = (e) => {
+		galleryContainer.classList.remove('close-galery');
+		galleryPhoto.src = e.target.src;
+		currentPhoto = index;
+		photos.forEach((element) => {
+			element.setAttribute('tabindex', -1);
+		});
+		// console.log(galleryWrapper);
+	};
+
+	photo.addEventListener('click', showPhotoInPopup);
+
+	// photo.addEventListener('keydown', (e) => {
+	// 	if (e.code === 'Enter' || e.keyCode === 13 || e.key === 'Enter') {
+	// 		showPhotoInPopup(e);
+	// 	}
+	// });
+});
